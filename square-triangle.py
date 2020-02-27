8660272e8ad6a0a759362773e4c8ebbc348b82f4
Bruno Henrique <30301681+brunohsn6@users.noreply.github.com>
2020-02-27 15:56:14 -0300

    changing background color

--- square-triangle.py
+++ square-triangle.py
@@ -52,7 +52,7 @@ def initialize():
 
 
 def render():
-    glClearColor(0, 0, 0, 1)
+    glClearColor(1, 1, 1, 1)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glUseProgram(shaderProgram)
